08f072cd8c056bbd9fe6cc0f34a1e922d87c3d5c
Enes Sür <surenesahmet@hotmail.com>
2023-10-18 18:04:23 +0300

    Upgrade Speed

--- Assets/Scripts/DragonController.cs
+++ Assets/Scripts/DragonController.cs
@@ -13,13 +13,15 @@ public class DragonController : FGroundFitter_MovementLook
     private Quaternion targetFlyRotation;
     private bool MoveForward;
     private float verticleFlySpeed = 0f;
-    private  FBasic_TPPCameraBehaviour tpp;
+    private FBasic_TPPCameraBehaviour tpp;
     [SerializeField] private GameObject _windPrefab;
+
+    public ParticleSystem particleSystem;
 
     protected override void Start()
     {
         base.Start();
-        
+
         clips.AddClip("JumpUp");
         clips.AddClip("FallToLanding");
         clips.AddClip("ToFly");
@@ -36,17 +38,14 @@ public class DragonController : FGroundFitter_MovementLook
     {
         base.Update();
         doubleClickTimer -= Time.deltaTime;
-        if (inAir)
-        {
-            WindEffect();
-        }
+
 
         if (MoveVector == Vector3.forward)
         {
             Debug.Log("MoveForward = true;");
             MoveForward = true;
         }
-        
+
         else
         {
             MoveForward = false;
@@ -55,13 +54,15 @@ public class DragonController : FGroundFitter_MovementLook
 
         if (flying)
         {
-            tpp.FollowingOffset = new Vector3(0f, Mathf.Lerp(tpp.FollowingOffset.y,1f,Time.deltaTime * 2f));
-            tpp.FollowingOffsetDirection = new Vector3(0f, 0f,Mathf.Lerp(tpp.FollowingOffsetDirection.z,-15f,Time.deltaTime * 2f));
+            tpp.FollowingOffset = new Vector3(0f, Mathf.Lerp(tpp.FollowingOffset.y, 1f, Time.deltaTime * 2f));
+            tpp.FollowingOffsetDirection = new Vector3(0f, 0f,
+                Mathf.Lerp(tpp.FollowingOffsetDirection.z, -15f, Time.deltaTime * 2f));
         }
         else
         {
-            tpp.FollowingOffset = new Vector3(0f, Mathf.Lerp(tpp.FollowingOffset.y,3f,Time.deltaTime * 2f));
-            tpp.FollowingOffsetDirection = new Vector3(0f, 0f,Mathf.Lerp(tpp.FollowingOffsetDirection.z,-7f,Time.deltaTime * 2f));
+            tpp.FollowingOffset = new Vector3(0f, Mathf.Lerp(tpp.FollowingOffset.y, 3f, Time.deltaTime * 2f));
+            tpp.FollowingOffsetDirection = new Vector3(0f, 0f,
+                Mathf.Lerp(tpp.FollowingOffsetDirection.z, -7f, Time.deltaTime * 2f));
         }
     }
 
@@ -73,25 +74,24 @@ public class DragonController : FGroundFitter_MovementLook
             {
                 if (YVelocity < -3f) CrossfadeTo("FallToLanding", 0.7f);
             }
-            else 
+            else
             {
                 if (ActiveSpeed > 0.15f)
                 {
                     if (Sprint)
                     {
-                         CrossfadeTo("Run", 0.25f);
+                        CrossfadeTo("Run", 0.25f);
                     }
                     else
                     {
                         CrossfadeTo("Walk", 0.25f);
                     }
-                } 
-                
+                }
+
                 else
                 {
                     CrossfadeTo("Idle", 0.25f);
                 }
-                
             }
         }
         else
@@ -111,7 +111,6 @@ public class DragonController : FGroundFitter_MovementLook
                     if (ActiveSpeed > BaseSpeed)
                     {
                         CrossfadeTo("Fly");
-                       
                     }
                     else
                     {
@@ -120,12 +119,13 @@ public class DragonController : FGroundFitter_MovementLook
                 }
             }
         }
+
         // If object is in air we just slowing animation speed to zero
         if (animatorHaveAnimationSpeedProp)
-            FAnimatorMethods.LerpFloatValue(animator, "AnimationSpeed", 
+            FAnimatorMethods.LerpFloatValue(animator, "AnimationSpeed",
                 MultiplySprintAnimation ? (ActiveSpeed / BaseSpeed) : Mathf.Min(1f, (ActiveSpeed / BaseSpeed)));
     }
-    
+
     protected override void HandleGravity()
     {
         if (!flying)
@@ -137,7 +137,7 @@ public class DragonController : FGroundFitter_MovementLook
             RaycastHit groundHit = fitter.CastRay();
             if (groundHit.transform)
             {
-                if (Mathf.Abs(groundHit.point.y  - transform.position.y) < 0.25f)
+                if (Mathf.Abs(groundHit.point.y - transform.position.y) < 0.25f)
                 {
                     flying = false;
                     RefreshHitGroundVars(groundHit);
@@ -145,14 +145,13 @@ public class DragonController : FGroundFitter_MovementLook
                     fitter.UpAxisRotation = transform.rotation.eulerAngles.y;
                 }
             }
-            
         }
     }
 
     protected override void HandleTransforming()
     {
         if (!flying)
-        { 
+        {
             base.HandleTransforming();
         }
         else
@@ -160,20 +159,24 @@ public class DragonController : FGroundFitter_MovementLook
             fitter.enabled = false;
             inAir = true;
             float? yAdjustPos = null;
-            targetFlyRotation = Camera.main.transform.rotation; 
-            targetFlyRotation = Quaternion.Euler(targetFlyRotation.eulerAngles.x - 15f, targetFlyRotation.eulerAngles.y + RotationOffset, targetFlyRotation.eulerAngles.z);
-    
+            targetFlyRotation = Camera.main.transform.rotation;
+            targetFlyRotation = Quaternion.Euler(targetFlyRotation.eulerAngles.x - 15f,
+                targetFlyRotation.eulerAngles.y + RotationOffset, targetFlyRotation.eulerAngles.z);
+
             if (MoveForward)
             {
                 if (!Sprint)
                 {
                     ActiveSpeed = Mathf.Lerp(ActiveSpeed, BaseSpeed * 4f, delta * AccelerationSpeed * 0.5f);
                     Debug.Log("Normal fly");
+                   
                 }
                 else
                 {
                     ActiveSpeed = Mathf.Lerp(ActiveSpeed, SprintingSpeed * 5f, delta * AccelerationSpeed * 0.35f);
                     Debug.Log("Sprint fly");
+                    particleSystem.Play();
+                    // Debug.Log(ActiveSpeed);
                 }
             }
             else
@@ -181,24 +184,27 @@ public class DragonController : FGroundFitter_MovementLook
                 Debug.Log("Stand in air");
                 ActiveSpeed = Mathf.Lerp(ActiveSpeed, -0.05f, Time.deltaTime * 0.75f);
                 if (ActiveSpeed < 0f) ActiveSpeed = 0f;
+                particleSystem.Stop();
             }
+
             transform.position += transform.forward * ActiveSpeed * delta;
-            transform.rotation = Quaternion.Slerp( transform.rotation, targetFlyRotation, Time.deltaTime * 3f);
-            
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetFlyRotation, Time.deltaTime * 3f);
+
             if (Input.GetKey(KeyCode.LeftControl)) ActiveSpeed = Mathf.Lerp(ActiveSpeed, 0f, Time.deltaTime * 4f);
             if (Input.GetKey(KeyCode.Space)) verticleFlySpeed = Mathf.Lerp(verticleFlySpeed, 1f, Time.deltaTime);
-            else 
-            if (Input.GetKey(KeyCode.Z)) verticleFlySpeed = Mathf.Lerp(verticleFlySpeed, -1f, Time.deltaTime);
+            else if (Input.GetKey(KeyCode.Z)) verticleFlySpeed = Mathf.Lerp(verticleFlySpeed, -1f, Time.deltaTime);
             else
             {
                 verticleFlySpeed = Mathf.Lerp(verticleFlySpeed, 0f, Time.deltaTime);
             }
+
             transform.position += Vector3.up * verticleFlySpeed * 20f * delta;
         }
     }
-    
-    
+
+
     float? yAdjustPos = null;
+
     protected override void ApplyTransforming()
     {
         if (!flying)
@@ -211,7 +217,7 @@ public class DragonController : FGroundFitter_MovementLook
             {
                 float yVelo = YVelocity;
 
-                if ( !inAir)
+                if (!inAir)
                     if (yAdjustPos != null)
                     {
                         yVelo = (yAdjustPos.Value - rigb.position.y) / Time.fixedDeltaTime;
@@ -223,8 +229,7 @@ public class DragonController : FGroundFitter_MovementLook
         }
     }
 
-    
-    
+
     public override void Jump()
     {
         base.Jump();
@@ -233,22 +238,26 @@ public class DragonController : FGroundFitter_MovementLook
         {
             StartFlying();
         }
+
         doubleClickTimer = 0.5f;
     }
 
-    
+
     private void StartFlying()
     {
         flying = true;
-       CrossfadeTo("Fly");
+        CrossfadeTo("Fly");
     }
 
-    private void WindEffect()
-    {
-        if (Input.GetKey(KeyCode.LeftShift) && (Input.GetKey(KeyCode.W)))
-        {
-            Instantiate(_windPrefab, transform.position, transform.rotation);
-           
-        }
-    }
+    // private void WindEffect()
+    // {
+    //     if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.W)) {
+    //         // Shift key is being pressed - enable particle system
+    //         particleSystem.Play();
+    //     } else {
+    //         // Shift key is not being pressed - disable particle system
+    //         
+    //         particleSystem.Stop();
+    //     }
+    // }
 }
